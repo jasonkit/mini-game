b06d708fc1ab466892c17a86c14fab0fa6d135f3
jasonkit <jasonkit@gmail.com>
2015-11-10 10:25:38 +0800

    remove unnecessary semicolon

--- tetris/js_src/game_timer.js
+++ tetris/js_src/game_timer.js
@@ -1,18 +1,17 @@
-function GameTimer (routine, interval)
-{
-	var self = this;
+function GameTimer(routine, interval) {
+  var self = this;
 
-	this.routine = routine;
-	this.interval = interval;
-	this.timer_id = null;
+  this.routine = routine;
+  this.interval = interval;
+  this.timer_id = null;
 
-	this.start = function() {
-		self.timer_id = setInterval(self.routine, self.interval);
-	};
+  this.start = function () {
+    self.timer_id = setInterval(self.routine, self.interval);
+  };
 
-	this.stop = function () {
-		if (self.timer_id !== null) {
-			clearInterval(self.timer_id);
-		}
-	};
-};
+  this.stop = function () {
+    if (self.timer_id !== null) {
+      clearInterval(self.timer_id);
+    }
+  };
+}
