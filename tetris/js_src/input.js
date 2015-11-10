b3467ff103b126d9a76b7982d2af627294a0f99e
jasonkit <jasonkit@gmail.com>
2015-11-10 10:27:51 +0800

    js formating

--- tetris/js_src/input.js
+++ tetris/js_src/input.js
@@ -1,37 +1,36 @@
 var Input = {
-	state: {},
+  state: {},
 };
 
 Input.get_state = function (identifier) {
-	if (identifier in Input.state) {
-		return Input.state[identifier];
-	}else {
-		return false;
-	}
+  if (identifier in Input.state) {
+    return Input.state[identifier];
+  } else {
+    return false;
+  }
 };
 
 Input.handle_keydown = function (e) {
-	Input.state[e.keyIdentifier] = true;
+  Input.state[e.keyIdentifier] = true;
 
-	var cust_handler = "handle_"+e.keyIdentifier+"_down";
-	
-	if ((cust_handler in Input) && (typeof Input[cust_handler] === "function")) {
-		Input[cust_handler]();
-	}
+  var cust_handler = "handle_" + e.keyIdentifier + "_down";
+
+  if ((cust_handler in Input) && (typeof Input[cust_handler] === "function")) {
+    Input[cust_handler]();
+  }
 };
 
 Input.handle_keyup = function (e) {
-	Input.state[e.keyIdentifier] = false;
+  Input.state[e.keyIdentifier] = false;
 
-	var cust_handler = "handle_"+e.keyIdentifier+"_up";
-	
-	if ((cust_handler in Input) && (typeof Input[cust_handler] === "function")) {
-		Input[cust_handler]();
-	}
+  var cust_handler = "handle_" + e.keyIdentifier + "_up";
+
+  if ((cust_handler in Input) && (typeof Input[cust_handler] === "function")) {
+    Input[cust_handler]();
+  }
 };
 
 Input.init = function () {
-	document.body.addEventListener("keydown", Input.handle_keydown);
-	document.body.addEventListener("keyup", Input.handle_keyup);
+  document.body.addEventListener("keydown", Input.handle_keydown);
+  document.body.addEventListener("keyup", Input.handle_keyup);
 };
-
